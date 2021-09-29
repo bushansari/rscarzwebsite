74059ee28f9c316e2fc8bc757e0518ac77a75365
bushansari <bushraansari393@gmail.com>
2021-09-29 21:49:58 +0530

    car mechanic

---
+++ assets/js/index.js
@@ -0,0 +1,95 @@
+//home1 page 
+
+$(document).ready(function () {
+    $(".my-car").slick({
+        autoplay: true,
+        dots: true,
+        speed: 1000,
+        customPaging: function (slider, i) {
+            varthumb = $(slider.$slides[i]).data();
+            return '<a>' + (i + 1) + '</a>';
+        },
+    })
+
+});
+
+
+// home5
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+
+// home6 page js brand logo slider
+//   all ------------------
+function initParadoxWay() {
+    "use strict";
+
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+
+            }
+        });
+    }
+
+    // bubbles -----------------
+
+
+    setInterval(function () {
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 4000, function () {
+            $(this).remove()
+        });
+    }, 350);
+
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
